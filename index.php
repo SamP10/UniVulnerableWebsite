0446c352d10bd9ada85ef59f66ce929fc58eb078
SamP10 <sam.plant998@gmail.com>
2021-01-29 16:34:43 +0000

    Authority added to index page for further redirects

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 include_once "init.php";
+
 if(isset($_SESSION["loggedin"])){
+    $id = $_SESSION['id'];
+    $sql= "SELECT priv_id FROM useraccounts WHERE (id=$id)";
+    $db = mysqli_query($connect, $sql);
+    while($row=mysqli_fetch_array($db)){
+        $authority = $row['priv_id'];
+        if($authority==1){
+            echo"You are IT";
+        }elseif($authority==2){
+            echo"You are Staff";
+        }elseif($authority==3){
+            echo"You are Student";
+        }else{
+            echo"Failed to understand authority";
+        }
+    }
+
 }else{
     header("location: login.php");
 }
