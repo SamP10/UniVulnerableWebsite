88e0d94c46e15b597abbd40ba4ce0646b4f6ce93
unknown <sam.plant998@gmail.com>
2021-01-30 23:27:59 +0000

    Working student lookup

--- StaffPages/studentLookup.php
+++ StaffPages/studentLookup.php
@@ -13,3 +13,99 @@ if(isset($_SESSION['loggedin'])){
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<legend>Student Lookup</legend>
+    <form action="" method="post">
+        By username:<input type="text" name="uname">
+        By first name:<input type="text" name="fname">
+        By surname:<input type="text" name="sname">
+        By student ID:<input type="text" name="student_id">
+        By date of birth:<input type="date" name="dob">
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+<?php
+if(!empty($_REQUEST['uname'])){
+    $var = $_REQUEST['uname'];
+    $sql="SELECT * FROM students WHERE username='$var'";
+    $run=mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_array($run)){
+        echo"Username: ".$row['username'];
+        echo"<br />First Name: ".$row['fname'];
+        echo"<br />Surname: ".$row['sname'];
+        echo"<br />Student ID: ".$row['student_id'];
+        echo"<br />D.O.B: ".$row['dob'];
+        echo"<br />Address: ".$row['address'];
+        echo"<br />Graduation Year: ".$row['graduation'];
+        echo"<br /><img src=''>";
+        echo"<br /><br />>";
+    }
+}elseif (!empty($_REQUEST['fname'])){
+    $var = $_REQUEST['fname'];
+    $sql="SELECT * FROM students WHERE fname='$var'";
+    $run=mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_array($run)){
+        echo"Username: ".$row['username'];
+        echo"<br />First Name: ".$row['fname'];
+        echo"<br />Surname: ".$row['sname'];
+        echo"<br />Student ID: ".$row['student_id'];
+        echo"<br />D.O.B: ".$row['dob'];
+        echo"<br />Address: ".$row['address'];
+        echo"<br />Graduation Year: ".$row['graduation'];
+        echo"<br /><img src=''>";
+        echo"<br /><br />>";
+    }
+}elseif (!empty($_REQUEST['sname'])){
+    $var = $_REQUEST['sname'];
+    $sql="SELECT * FROM students WHERE sname=$var";
+    $run=mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_array($run)){
+        echo"Username: ".$row['username'];
+        echo"<br />First Name: ".$row['fname'];
+        echo"<br />Surname: ".$row['sname'];
+        echo"<br />Student ID: ".$row['student_id'];
+        echo"<br />D.O.B: ".$row['dob'];
+        echo"<br />Address: ".$row['address'];
+        echo"<br />Graduation Year: ".$row['graduation'];
+        echo"<br /><img src=''>";
+        echo"<br /><br />>";
+    }
+}elseif (!empty($_REQUEST['student_id'])){
+    $var = $_REQUEST['student_id'];
+    $sql="SELECT * FROM students WHERE student_id=$var";
+    $run=mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_array($run)){
+        echo"Username: ".$row['username'];
+        echo"<br />First Name: ".$row['fname'];
+        echo"<br />Surname: ".$row['sname'];
+        echo"<br />Student ID: ".$row['student_id'];
+        echo"<br />D.O.B: ".$row['dob'];
+        echo"<br />Address: ".$row['address'];
+        echo"<br />Graduation Year: ".$row['graduation'];
+        echo"<br /><img src=''>";
+        echo"<br /><br />>";
+    }
+}elseif (!empty($_REQUEST['dob'])){
+    $var = $_REQUEST['dob'];
+    $sql="SELECT * FROM students WHERE dob=$var";
+    $run=mysqli_query($connect, $sql);
+    while($row = mysqli_fetch_array($run)){
+        echo"Username: ".$row['username'];
+        echo"<br />First Name: ".$row['fname'];
+        echo"<br />Surname: ".$row['sname'];
+        echo"<br />Student ID: ".$row['student_id'];
+        echo"<br />D.O.B: ".$row['dob'];
+        echo"<br />Address: ".$row['address'];
+        echo"<br />Graduation Year: ".$row['graduation'];
+        echo"<br /><img src=''>";
+        echo"<br /><br />>";
+    }
+}else{
+    echo"No Student Match";
+}
