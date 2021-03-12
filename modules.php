d3ddee33634b74b57fa72cab62cbe24ef7b7d279
SamP10 <72082567+SamP10@users.noreply.github.com>
2021-03-12 13:36:08 +0000

    Update modules.php

--- modules.php
+++ modules.php
@@ -1,16 +1,7 @@
 <?php
 include_once "init.php";
 include "navBar.php";
-if($_SESSION['priv_id']==2){
-        $username=$_SESSION['username'];
-        $sql="SELECT staff_id FROM staff WHERE username='$username'";
-        $query=mysqli_query($connect, $sql);
 
-        $result=$query->fetch_row();
-        $staff_id=$result[0];
-        $_SESSION['staff_id']=$staff_id;
-        header('location: modulesDisplay.php');
-    }
 ?>
 <div class="container">
 <?php
@@ -28,7 +19,24 @@ if(isset($_SESSION['loggedin'])){
         while($row=mysqli_fetch_array($run)){
             $_SESSION['course_id']=$course_id;
             echo "<h3>Course: <a href='./modulesDisplay.php'>".$row['course_name']."</a></h3>";
+        }elseif($_SESSION['priv_id']==2){
+        $username=$_SESSION['username'];
+        $sql="SELECT staff_id FROM staff WHERE username='$username'";
+        $query=mysqli_query($connect, $sql);
+
+        $result=$query->fetch_row();
+        $staff_id=$result[0];
+        $_SESSION['staff_id']=$staff_id;        
+        $data="SELECT * FROM modules WHERE module_lead_id=$staff_id";
+        $run=mysqli_query($connect, $data);
+        echo"<h1>Your modules</h1>";
+        while($row=mysqli_fetch_assoc($run)){
+                echo "<form method='POST' action='./moduleView.php'>";
+                echo "<h5>".$row['module_name']."</h5>";
+                echo "<button type='submit' name='module_id' value='".$row['module_id']."'>VIEW</button><br>";
+                echo "</form>";
         }
+    }
 }
 }
 ?>
