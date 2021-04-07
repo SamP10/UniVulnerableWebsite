ef17e41dee9bbeb6ca7939bdaa103ed6b91cf8eb
SamP10 <72082567+SamP10@users.noreply.github.com>
2021-04-07 16:20:56 +0100

    Update editDetails.php

--- editDetails.php
+++ editDetails.php
@@ -79,7 +79,7 @@ if(isset($_SESSION['loggedin'])){
             $id=$_POST['updateP'];
 
             if ($newpassword == $confPassword) {
-                $hashedPassword = crypt($newpassword, '$1$mysalt$');
+                $hashedPassword = password_hash($newpassword, PASSWORD_DEFAULT);
                 $update = "UPDATE useraccounts SET password='$hashedPassword' WHERE user_id=$id";
                 mysqli_query($connect, $update);
 
