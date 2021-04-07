6fc7bae14c36bd87096b5eae360fe3a753f5af4e
SamP10 <72082567+SamP10@users.noreply.github.com>
2021-04-07 17:05:16 +0100

    Update accountEdit.php

--- accountEdit.php
+++ accountEdit.php
@@ -23,17 +23,17 @@ echo"<form action='' method='post'>";
 echo"NEW PASSWORD: <input name='newPassword' type='password'><br>";
 echo"CONFIRM PASSWORD: <input name='confPassword' type='password'>";
 echo "<input type='hidden' name='id' value='".$id."'><br>";
-echo"<button type='submit' name='updateP'>UPDATE</button>";
+echo"<button type='submit' name='updateP'  value='".$id."'>UPDATE</button>";
 echo"</form>";
 
 if(isset($_POST['updateP'])) {
     if (!empty($_POST['newPassword']) && !empty($_POST['confPassword'])) {
         $newpassword = $_POST['newPassword'];
         $confPassword = $_POST['confPassword'];
-        $id = $_POST['id'];
+        $uid = $_POST['updateP'];
         if ($newpassword == $confPassword) {
             $hashedPassword =  password_hash($newpassword, PASSWORD_DEFAULT);
-            $update = "UPDATE useraccounts SET password='$hashedPassword' WHERE user_id=$id";
+            $update = "UPDATE useraccounts SET password='$hashedPassword' WHERE user_id=$uid";
             mysqli_query($connect, $update);
         } else {
             echo "Passwords do not match!";
